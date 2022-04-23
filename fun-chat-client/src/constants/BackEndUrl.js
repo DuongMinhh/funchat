01b0b48d1121d211c1ea5dc15590225d674c1fc4
DuongMinh <nguyenminhduong2310@gmail.com>
2022-04-23 23:41:39 +0700

    Setup env for FE

--- fun-chat-client/src/constants/BackEndUrl.js
+++ fun-chat-client/src/constants/BackEndUrl.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8080/fun-chat/api'
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
+export const API_BASE_URL = `${process.env.REACT_APP_BE_URL}/fun-chat/api`
+export const OAUTH2_REDIRECT_URI = `${process.env.REACT_APP_FE_URL}/oauth2/redirect`
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
